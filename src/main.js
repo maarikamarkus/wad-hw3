13951b01c231765c537585875c6ab918c5352763
maarikamarkus <maarika.markus@gmail.com>
2020-11-18 11:27:42 +0200

    added an example for routing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import HelloWorld from "@/components/HelloWorld"
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  {path: "/helloworld", component: HelloWorld}
+];
+
+const router = new VueRouter({
+  routes
+});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
