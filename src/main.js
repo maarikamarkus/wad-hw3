5f0155da3598b68bae305cf3672b47d7069fe3da
maarikamarkus <maarika.markus@gmail.com>
2020-11-18 12:00:34 +0200

    added some vuex example as comments

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import HelloWorld from "@/components/HelloWorld"
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 const routes = [
   {path: "/helloworld", component: HelloWorld}
@@ -14,7 +16,15 @@ const router = new VueRouter({
   routes
 });
 
+const store = new Vuex.Store({
+  //state: {someVariable = ''},
+  //mutation: {function: function(state) {state.someVariable++}},
+  //actions: {increment (context) { context.commit('functionFromMutation')} }
+  //getters: {getSomeVariable: function(state) {return state.someVariable}},
+});
+
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
