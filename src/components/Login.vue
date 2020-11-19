7691ea96afd97b66911db4b6d0537f40d4d8901d
maarikamarkus <maarika.markus@gmail.com>
2020-11-19 14:25:18 +0200

    moved button css to App component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -68,18 +68,4 @@ export default {
     padding: 8px 16px;
     margin: 4px 0;
 }
-
-button {
-    padding: 8px 16px;
-    margin: 4px 0;
-    color: #ffffff;
-    background-color: #01579b;
-    border: none;
-    border-radius: 4px;
-}
-
-button:hover {
-    box-shadow: 0 0 5px rgba(38, 50, 56, 0.7);
-    cursor: pointer;
-}
 </style>>
