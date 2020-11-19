ed75a785a997faf3ce404cdbdce74cdb67dc07f4
maarikamarkus <maarika.markus@gmail.com>
2020-11-19 14:25:04 +0200

    added content to browse, needs some css fixing

--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -1,13 +1,65 @@
 <template>
-    <h1>Browse</h1>
+    <section class="main-container">
+        <div v-for="user in users" :key="user.id">
+            <div class="profile">
+                <img :src="user.avatar" :alt="user.firstname + ' ' + user.lastname">
+                <h2> {{ user.firstname }} {{ user.lastname }} </h2>
+                <button class="follow-button">Follow</button>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
 export default {
-    
+    name: 'Browse',
+    data () {
+        return {}
+    },
+    methods: {
+
+    },
+    computed: {
+        users () {
+            return this.$store.state.users;
+        },
+    },
+    mounted () {
+        this.$store.dispatch('getUsers');
+    },
 }
 </script>
 
 <style scoped>
+.profile {
+    width: 45%;
+    display: inline-block;
+    border: 1px solid #dedede;
+    border-radius: 5px;
+    text-align: center;
+    margin: 1%;
+}
 
+.profile img {
+    width: 75px;
+    height: 75px;
+    border-radius: 100%;
+    object-fit: cover;
+    object-position: top;
+    margin: 5px;
+}
+
+.profile h2 {
+    font-size: 16px;
+}
+
+.follow-button {
+    background-color: #82008f;
+}
+
+.follow-button.followed{
+    background-color: #ffffff;
+    border: 1px solid #82008f;
+    color: #82008f;
+}
 </style>
